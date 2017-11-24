828abe0a29113f4982331649880f56858ee4b6ce
ghjhhyuyuy <798389920@qq.com>
2017-11-24 21:10:18 +0800

    Add files via upload
    更新成了窗体面板

--- team1/UI.java
+++ team1/UI.java
@@ -1,17 +1,47 @@
 package team1;
 
-import java.util.Scanner;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class UI {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		FlowLayoutWindow window =new FlowLayoutWindow();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setVisible(true);
+	}
+	
+}
+class FlowLayoutWindow extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3937967043739601992L;
+
+	FlowLayoutWindow(){
+		setLayout(new FlowLayout());	
 		User user = new User();
 		MyFind find = new MyFind();
-		System.out.println("请输入账户");
-		user.setAccount(in.next());
-		System.out.println("请输入密码");
-		user.setPsw(in.next());
-		String res = find.findInFile(user);
-		System.out.println(res);
+		JTextField text = new JTextField("账户",10);
+		JTextField text1 = new JTextField("密码",10);
+		add(text);
+		add(text1);
+		JButton button = new JButton("ok");
+		add(button);
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO 自动生成的方法存根
+				user.setAccount(text.getText());
+				user.setPsw(text1.getText());
+				String res = find.findInFile(user);
+     			System.out.println(res);
+			}	
+		    });
 	}
 }
